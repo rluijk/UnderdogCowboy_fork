a1dcb1981016139be8c57955c5edc9b875a58827
Rene Luijk <rluijk@gmail.com>
2024-07-13 10:59:48 +0800

    Migrate package metadata from setup.py to pyproject.toml
    This commit moves the package dependencies from setup.py to pyproject.toml to enable automatic pip install of dependencies for the local development of the package. By specifying the dependencies in pyproject.toml, they can be automatically installed when running pip install -e . in the project root directory. This eliminates the need for a separate requirements.txt file for local development.

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ setup(
     name="underdogcowboy",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-    ],
     entry_points={
         "console_scripts": [
             "underdogcowboy=underdogcowboy.main:main",
         ],
     },
 )
+
