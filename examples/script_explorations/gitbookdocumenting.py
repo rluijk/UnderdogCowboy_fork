30890f6e762bdb6cbb4703395d69a5fe2f6109f1
Rene Luijk <rluijk@gmail.com>
2024-08-13 08:06:39 +0800

    explorations

--- examples/script_explorations/gitbookdocumenting.py
+++ examples/script_explorations/gitbookdocumenting.py
@@ -5,20 +5,19 @@
 
 from underdogcowboy import adm, gitbookdoc
 
-    # Enable dialog with the agent via the Agent Dialog Manager (adm)
-    adm | [gitbookdoc]
+# Enable dialog with the agent via the Agent Dialog Manager (adm)
+adm | [gitbookdoc]
 
-    # Ask the agent to make some documentation.
-    # In this case about this script as we see here, since this will allow
-    # it to have a small initial understanding and 
-    # conversation about our small DSL as used in this script
+# Ask the agent to make some documentation.
+# In this case about this script as we see here, since this will allow
+# it to have a small initial understanding and 
+# conversation about our small DSL as used in this script
 
-    # The file reference to this script
-    file_ref = "/Users/reneluijk/projects/UnderdogCowboy/examples/script_explorations/gitbookdocumenting.py"
+# The file reference to this script
+file_ref = "/Users/reneluijk/projects/UnderdogCowboy/examples/script_explorations/gitbookdocumenting.py"
 
-    # Send the script to the agent (the LLM)
-    response = gitbookdoc >> f"file {file_ref}"
-    print(response)
+# Send the script to the agent (the LLM)
+
 
 # Put the Agent Dialog Manager interactive mode (+adm) to continue the conversation with the gitbookdoc agent
 # in the command line interface (CLI).
