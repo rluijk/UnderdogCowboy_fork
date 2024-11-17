d37b7e1e003d9ef9b9efecedafe5e6eb46ea8e84
Rene Luijk <rluijk@gmail.com>
2024-11-18 07:41:20 +0800

    part of event driven refactor (from reactive)

--- underdogcowboy/core/commandtools/agent_flow/ui_components/category_editor_ui.py
+++ underdogcowboy/core/commandtools/agent_flow/ui_components/category_editor_ui.py
@@ -9,19 +9,17 @@ from ui_components.session_dependent import SessionDependentUI
 # LLM Related
 from llm_call_manager import LLMCallManager
 
-
 class CategoryEditorUI(SessionDependentUI):
-
 
     def __init__(self, session_manager, screen_name, agent_name_plain):
         super().__init__(session_manager, screen_name, agent_name_plain)
         self.session_manager = session_manager
         self.selected_category = self.session_manager.get_data('selected_category', "Default Category")
-        logging.info(f"selected at init of CategoryEditorUI: {self.selected_category}")
         self.styles.overflow_y = "auto"  # Enable vertical scrolling
         self.styles.height = "500"       # Set a fixed height
         self.agent_name_plain = agent_name_plain
 
     def compose(self) -> ComposeResult:
-        yield CategoryScaleWidget(self.agent_name_plain)
+        logging.info("Composing: CategoryScaleWidget")
+        yield CategoryScaleWidget(self.session_manager, self.screen_name,self.agent_name_plain)
 
